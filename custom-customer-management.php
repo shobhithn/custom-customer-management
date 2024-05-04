f599e1f460b48d83b19841b7a5bba6448be7f0b1
SHOBHITH N <shobith@idaksh.in>
2024-05-04 16:38:50 +0530

    first commit

---
+++ custom-customer-management.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Custom Customer Management
+Description: This plugin provides CRUD functionality for managing customer details with separate database tables.
+Version: 1.0
+Author: SHOBHITH N
+*/
